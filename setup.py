7e837efbf77b041b98bbcb5107b77928b6ed992f
Mikel Penagarikano <mpenagar@yahoo.es>
2026-02-03 16:48:05 +0100

    Add setup.py for package configuration

---
+++ setup.py
@@ -0,0 +1,17 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="odyssey_2026",             # Nombre de importación
+    version="0.1",
+    packages=find_packages(),       # Busca automáticamente la carpeta 'odyssey_2026'
+    package_data={
+        'odyssey_2026': ['data/*.npz']  # Busca dentro de la carpeta data
+    },
+    install_requires=[              # Dependencias automáticas (Opcional)
+        "numpy",
+        "matplotlib",
+        "scikit-learn",
+        "scipy"
+    ]
+)
