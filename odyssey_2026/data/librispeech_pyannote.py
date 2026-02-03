0a3490b3a6fb12ef0f1f11629e96cbadc339ed0a
Mikel Penagarikano <mpenagar@yahoo.es>
2026-02-03 17:54:19 +0100

    Rename file and add data download functionality

--- odyssey_2026/data/librispeech_pyannote.py
+++ odyssey_2026/data/librispeech_pyannote.py
@@ -1,13 +1,21 @@
-# odyssey_2026/data/librispeech-pyannote_embeddings.py
+# odyssey_2026/data/librispeech_pyannote.py
 import os
 import numpy as np
+import requests
 
-# 1. Ruta relativa a ESTE archivo
 _path = os.path.join(os.path.dirname(__file__), 'librispeech_pyannote.npz')
+_URL_DESCARGA = "https://github.com/mpenagar/odyssey_2026_python/releases/download/latest/librispeech_pyannote.npz"
 
-# 2. Carga inmediata al importar este módulo específico
-print("Cargando LibriSpeech...")
+def _download_data():
+    print(f"Descargando dataset a {_path} ...")
+    response = requests.get(_URL_DESCARGA, stream=True)
+    with open(_path, 'wb') as f:
+        for chunk in response.iter_content(chunk_size=8192):
+            f.write(chunk)
+    print("Descarga completada.")
+
+_download_data()
 _data = np.load(_path, allow_pickle=True)
 
 embeddings = _data['X']
-spk_ids = _data['y']
+spk_ids =    _data['y']
