9a3d6f65c3feacf7f732261d52bca9dba8b2f655
Mikel Penagarikano <mpenagar@yahoo.es>
2026-02-05 10:54:10 +0100

    Delete odyssey_2026/data/librispeech_dev_clean_pyannote_legacy.py

--- odyssey_2026/data/librispeech_dev_clean_pyannote_legacy.py
+++
@@ -1,9 +0,0 @@
-from ._utils import download_data
-from pathlib import Path
-import numpy as np
-
-_path = download_data(Path(__file__).stem)
-_data = np.load(_path, allow_pickle=True)
-
-embeddings = _data['embeddings']
-spk_ids =    _data['speakers']
