b1aca65b260c832d2d9594d34156de3c51df4589
Mikel Penagarikano <mpenagar@yahoo.es>
2026-02-05 11:32:03 +0100

    Remove package_data from setup.py
    Removed package_data configuration for data files.

--- setup.py
+++ setup.py
@@ -5,9 +5,6 @@ setup(
     name="odyssey_2026",             # Nombre de importación
     version="0.1",
     packages=find_packages(),       # Busca automáticamente la carpeta 'odyssey_2026'
-    package_data={
-        'odyssey_2026': ['data/*.npz']  # Busca dentro de la carpeta data
-    },
     install_requires=[              # Dependencias automáticas (Opcional)
         "numpy",
         "matplotlib",
