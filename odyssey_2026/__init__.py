45e8fd23548e9e9d18ae52e100f3844dd1066ce7
Mikel Penagarikano <mpenagar@yahoo.es>
2026-02-06 15:46:14 +0100

    Add seed import from deterministic module

--- odyssey_2026/__init__.py
+++ odyssey_2026/__init__.py
@@ -2,3 +2,4 @@
 
 # Importamos las funciones de core.py para que sean accesibles directamente
 from .eval import target_scores, get_eer, plot_hist, plot_det_curve, train_test_y_split
+from .deterministic import seed
